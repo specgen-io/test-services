4ac565d0067159872a9caeef6e85756cd3b4c9c3
Anastasiya Nislovskaya <anastasiya@specgen.io>
2021-08-24 20:24:17 +0300

    service-go: Added sevices implementation

--- go/services/check_service.go
+++ go/services/check_service.go
@@ -1,6 +1,5 @@
 package services
 
-import "errors"
 import "cloud.google.com/go/civil"
 import "github.com/google/uuid"
 import "github.com/shopspring/decimal"
@@ -9,14 +8,33 @@ import "test-service/spec"
 type CheckService struct{}
 
 func (service *CheckService) CheckEmpty() (*spec.CheckEmptyResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+	return &spec.CheckEmptyResponse{Ok: &spec.Empty}, nil
 }
-func (service *CheckService) CheckQuery(pString string, pStringOpt *string, pStringArray []string, pDate civil.Date, pDateArray []civil.Date, pDatetime civil.DateTime, pInt int, pLong int64, pDecimal decimal.Decimal, pEnum spec.Choice, pStringDefaulted string) (*spec.CheckQueryResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+func (service *CheckService) CheckQuery(pString string,
+	pStringOpt *string,
+	pStringArray []string,
+	pDate civil.Date,
+	pDateArray []civil.Date,
+	pDatetime civil.DateTime,
+	pInt int,
+	pLong int64,
+	pDecimal decimal.Decimal,
+	pEnum spec.Choice,
+	pStringDefaulted string) (*spec.CheckQueryResponse, error) {
+
+	return &spec.CheckQueryResponse{Ok: &spec.Empty}, nil
 }
-func (service *CheckService) CheckUrlParams(intUrl int64, stringUrl string, floatUrl float32, boolUrl bool, uuidUrl uuid.UUID, decimalUrl decimal.Decimal, dateUrl civil.Date) (*spec.CheckUrlParamsResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+func (service *CheckService) CheckUrlParams(
+	intUrl int64,
+	stringUrl string,
+	floatUrl float32,
+	boolUrl bool,
+	uuidUrl uuid.UUID,
+	decimalUrl decimal.Decimal,
+	dateUrl civil.Date) (*spec.CheckUrlParamsResponse, error) {
+
+	return &spec.CheckUrlParamsResponse{Ok: &spec.Empty}, nil
 }
 func (service *CheckService) CheckForbidden() (*spec.CheckForbiddenResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+	return &spec.CheckForbiddenResponse{Forbidden: &spec.Empty}, nil
 }
--- go/services/echo_service.go
+++ go/services/echo_service.go
@@ -1,19 +1,18 @@
 package services
 
-import "errors"
 import "test-service/spec"
 
 type EchoService struct{}
 
 func (service *EchoService) EchoBody(body *spec.Message) (*spec.EchoBodyResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+	return &spec.EchoBodyResponse{Ok: body}, nil
 }
 func (service *EchoService) EchoQuery(intQuery int, stringQuery string) (*spec.EchoQueryResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+	return &spec.EchoQueryResponse{Ok: &spec.Message{IntField: intQuery, StringField: stringQuery}}, nil
 }
 func (service *EchoService) EchoHeader(intHeader int, stringHeader string) (*spec.EchoHeaderResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+	return &spec.EchoHeaderResponse{Ok: &spec.Message{IntField: intHeader, StringField: stringHeader}}, nil
 }
 func (service *EchoService) EchoUrlParams(intUrl int, stringUrl string) (*spec.EchoUrlParamsResponse, error) {
-	return nil, errors.New("implementation has not added yet")
+	return &spec.EchoUrlParamsResponse{Ok: &spec.Message{IntField: intUrl, StringField: stringUrl}}, nil
 }
