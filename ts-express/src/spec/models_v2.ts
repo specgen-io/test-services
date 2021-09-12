9066810a9c25d5afa0ceec81d04a404cbfb54550
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-09-11 23:05:04 -0400

    Removed generated code

--- ts-express/src/spec/models_v2.ts
+++
@@ -1,8 +0,0 @@
-import * as t from './superstruct'
-
-export const TMessage = t.type({
-    bool_field: t.boolean(),
-    string_field: t.string(),
-})
-
-export type Message = t.Infer<typeof TMessage>
