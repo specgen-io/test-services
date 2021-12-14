08bdded6b94195780c78f6307b9c1716a16cda06
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-12-13 20:53:26 -0500

    Make tests great again

--- tests/service_test.go
+++ tests/service_test.go
@@ -10,66 +10,52 @@ import (
 
 var serviceUrl = "http://localhost:8081"
 
-func Test_Echo_Body_String(t *testing.T) {
-	dataText := "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu"
-
-	req, err := http.NewRequest("POST", serviceUrl+`/echo/body_string`, strings.NewReader(dataText))
-	assert.NilError(t, err)
-
-	req.Header.Set("Content-Type", "text/plain")
-
+func assertResponseSuccess(t *testing.T, req *http.Request, expectedStatusCode int, expectedBody string) {
 	resp, err := http.DefaultClient.Do(req)
 	assert.NilError(t, err)
 
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
+	assert.Equal(t, resp.StatusCode, expectedStatusCode)
 
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, string(body), dataText)
+	if expectedBody != "" {
+		body, err := ioutil.ReadAll(resp.Body)
+		assert.NilError(t, err)
+		err = resp.Body.Close()
+		assert.NilError(t, err)
+		assert.Equal(t, strings.TrimSuffix(string(body), "\n"), expectedBody)
+	}
+}
+
+func Test_Echo_Body_String(t *testing.T) {
+	dataText := "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu"
+
+	req, _ := http.NewRequest("POST", serviceUrl+`/echo/body_string`, strings.NewReader(dataText))
+	req.Header.Set("Content-Type", "text/plain")
+
+	assertResponseSuccess(t, req, 200, dataText)
 }
 
 func Test_Echo_Body(t *testing.T) {
 	dataJson := `{"int_field":123,"string_field":"the value"}`
 
-	req, err := http.NewRequest("POST", serviceUrl+`/echo/body`, strings.NewReader(dataJson))
-	assert.NilError(t, err)
-
+	req, _ := http.NewRequest("POST", serviceUrl+`/echo/body`, strings.NewReader(dataJson))
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, strings.TrimSuffix(string(body), "\n"), dataJson)
+	assertResponseSuccess(t, req, 200, dataJson)
 }
 
 func Test_Echo_Body_Bad_Request(t *testing.T) {
 	dataJson := `{"int_field":"the string","string_field":"the value"}`
 
-	req, err := http.NewRequest("POST", serviceUrl+`/echo/body`, strings.NewReader(dataJson))
-	assert.NilError(t, err)
-
+	req, _ := http.NewRequest("POST", serviceUrl+`/echo/body`, strings.NewReader(dataJson))
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 400)
+	assertResponseSuccess(t, req, 400, "")
 }
 
 func Test_Echo_Query_Params(t *testing.T) {
 	dataJson := `{"int_field":123,"long_field":12345,"float_field":1.23,"double_field":12.345,"decimal_field":12345,"bool_field":true,"string_field":"the value","string_opt_field":"the value","string_defaulted_field":"value","string_array_field":["the str1","the str2"],"uuid_field":"123e4567-e89b-12d3-a456-426655440000","date_field":"2020-01-01","date_array_field":["2020-01-01","2020-01-02"],"datetime_field":"2019-11-30T17:45:55","enum_field":"SECOND_CHOICE"}`
 
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/query`, nil)
-	assert.NilError(t, err)
-
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/query`, nil)
 	q := req.URL.Query()
 	q.Add("int_query", "123")
 	q.Add("long_query", "12345")
@@ -90,48 +76,29 @@ func Test_Echo_Query_Params(t *testing.T) {
 	q.Add("enum_query", "SECOND_CHOICE")
 	req.URL.RawQuery = q.Encode()
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, strings.TrimSuffix(string(body), "\n"), dataJson)
+	assertResponseSuccess(t, req, 200, dataJson)
 }
 
 func Test_Echo_Query_Params_Bad_Request(t *testing.T) {
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/query`, nil)
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/query`, nil)
 	q := req.URL.Query()
 	q.Add("int_query", "the value")
 	q.Add("string_query", "the value")
 	req.URL.RawQuery = q.Encode()
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 400)
+	assertResponseSuccess(t, req, 400, "")
 }
 
 func Test_Echo_Query_Params_Missing(t *testing.T) {
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/query`, nil)
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/query`, nil)
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 400)
+	assertResponseSuccess(t, req, 400, "")
 }
 
 func Test_Echo_Header_Params(t *testing.T) {
 	dataJson := `{"int_field":123,"long_field":12345,"float_field":1.23,"double_field":12.345,"decimal_field":12345,"bool_field":true,"string_field":"the value","string_opt_field":"the value","string_defaulted_field":"value","string_array_field":["the str1","the str2"],"uuid_field":"123e4567-e89b-12d3-a456-426655440000","date_field":"2020-01-01","date_array_field":["2020-01-01","2020-01-02"],"datetime_field":"2019-11-30T17:45:55","enum_field":"SECOND_CHOICE"}`
 
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/header`, nil)
-	assert.NilError(t, err)
-
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/header`, nil)
 	h := req.Header
 	h.Add("Int-Header", "123")
 	h.Add("Long-Header", "12345")
@@ -151,122 +118,63 @@ func Test_Echo_Header_Params(t *testing.T) {
 	h.Add("Datetime-Header", "2019-11-30T17:45:55")
 	h.Add("Enum-Header", "SECOND_CHOICE")
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, strings.TrimSuffix(string(body), "\n"), dataJson)
+	assertResponseSuccess(t, req, 200, dataJson)
 }
 
 func Test_Echo_Header_Params_Bad_Request(t *testing.T) {
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/header`, nil)
-	assert.NilError(t, err)
-	h := req.Header
-	h.Add("Int-Header", "the value")
-	h.Add("String-Header", "the value")
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/header`, nil)
+	req.Header.Add("Int-Header", "the value")
+	req.Header.Add("String-Header", "the value")
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 400)
+	assertResponseSuccess(t, req, 400, "")
 }
 
 func Test_Echo_Url_Params(t *testing.T) {
 	dataJson := `{"int_field":123,"long_field":12345,"float_field":1.23,"double_field":12.345,"decimal_field":12345,"bool_field":true,"string_field":"the value","uuid_field":"123e4567-e89b-12d3-a456-426655440000","date_field":"2020-01-01","datetime_field":"2019-11-30T17:45:55","enum_field":"SECOND_CHOICE"}`
 
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/url_params/123/12345/1.23/12.345/12345/true/the value/123e4567-e89b-12d3-a456-426655440000/2020-01-01/2019-11-30T17:45:55/SECOND_CHOICE`, nil)
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/url_params/123/12345/1.23/12.345/12345/true/the value/123e4567-e89b-12d3-a456-426655440000/2020-01-01/2019-11-30T17:45:55/SECOND_CHOICE`, nil)
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, strings.TrimSuffix(string(body), "\n"), dataJson)
+	assertResponseSuccess(t, req, 200, dataJson)
 }
 
 func Test_Echo_Url_Params_Bad_Request(t *testing.T) {
-	req, err := http.NewRequest("GET", serviceUrl+`/echo/url_params/value/12345/1.23/12.345/12345/true/the value/123e4567-e89b-12d3-a456-426655440000/2020-01-01/2019-11-30T17:45:55/SECOND_CHOICE`, nil)
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("GET", serviceUrl+`/echo/url_params/value/12345/1.23/12.345/12345/true/the value/123e4567-e89b-12d3-a456-426655440000/2020-01-01/2019-11-30T17:45:55/SECOND_CHOICE`, nil)
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 400)
+	assertResponseSuccess(t, req, 400, "")
 }
 
 func Test_Echo_Everything(t *testing.T) {
 	dataJson := `{"body_field":{"int_field":123,"string_field":"the value"},"float_query":1.23,"bool_query":true,"uuid_header":"123e4567-e89b-12d3-a456-426655440000","datetime_header":"2019-11-30T17:45:55","date_url":"2020-01-01","decimal_url":12345}`
 
-	req, err := http.NewRequest("POST", serviceUrl+`/echo/everything/2020-01-01/12345`, strings.NewReader(`{"int_field":123,"string_field":"the value"}`))
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("POST", serviceUrl+`/echo/everything/2020-01-01/12345`, strings.NewReader(`{"int_field":123,"string_field":"the value"}`))
 	req.Header.Set("Content-Type", "application/json")
-
+	req.Header.Add("Uuid-Header", "123e4567-e89b-12d3-a456-426655440000")
+	req.Header.Add("Datetime-Header", "2019-11-30T17:45:55")
 	q := req.URL.Query()
 	q.Add("float_query", "1.23")
 	q.Add("bool_query", "true")
 	req.URL.RawQuery = q.Encode()
 
-	h := req.Header
-	h.Add("Uuid-Header", "123e4567-e89b-12d3-a456-426655440000")
-	h.Add("Datetime-Header", "2019-11-30T17:45:55")
-
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, strings.TrimSuffix(string(body), "\n"), dataJson)
+	assertResponseSuccess(t, req, 200, dataJson)
 }
 
 func Test_Check_Response_Empty(t *testing.T) {
-	req, err := http.NewRequest("GET", serviceUrl+`/check/empty`, nil)
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("GET", serviceUrl+`/check/empty`, nil)
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
+	assertResponseSuccess(t, req, 200, "")
 }
 
 func Test_Check_Response_Forbidden(t *testing.T) {
-	req, err := http.NewRequest("GET", serviceUrl+`/check/forbidden`, nil)
-	assert.NilError(t, err)
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
+	req, _ := http.NewRequest("GET", serviceUrl+`/check/forbidden`, nil)
 
-	assert.Equal(t, resp.StatusCode, 403)
+	assertResponseSuccess(t, req, 403, "")
 }
 
 func Test_Echo_Body_V2(t *testing.T) {
 	dataJson := `{"bool_field":true,"string_field":"the value"}`
 
-	req, err := http.NewRequest("POST", serviceUrl+`/v2/echo/body`, strings.NewReader(dataJson))
-	assert.NilError(t, err)
-
+	req, _ := http.NewRequest("POST", serviceUrl+`/v2/echo/body`, strings.NewReader(dataJson))
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := http.DefaultClient.Do(req)
-	assert.NilError(t, err)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	assert.NilError(t, err)
-	err = resp.Body.Close()
-	assert.NilError(t, err)
-
-	assert.Equal(t, resp.StatusCode, 200)
-	assert.Equal(t, strings.TrimSuffix(string(body), "\n"), dataJson)
+	assertResponseSuccess(t, req, 200, dataJson)
 }
