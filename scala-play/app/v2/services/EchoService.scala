05bc86e8c152c90804cf306950c37fb3f1cda33e
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-12-22 12:21:48 -0500

    scala-play: Fix for compilation

--- scala-play/app/v2/services/EchoService.scala
+++ scala-play/app/v2/services/EchoService.scala
@@ -1,12 +1,12 @@
-package services.v2
+package v2.services.echo
 
 import javax.inject._
 import scala.concurrent._
-import models.v2._
+import v2.services.echo._
+import v2.models._
 
 @Singleton
 class EchoService @Inject()()(implicit ec: ExecutionContext) extends IEchoService {
-  import IEchoService._
   override def echoBody(body: Message): Future[EchoBodyResponse] = Future {
     EchoBodyResponse.Ok(Message(body.boolField, body.stringField))
   }
