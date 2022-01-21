794b079958acc301d8bb114b341a3bdcd76fa24e
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2022-01-21 15:14:57 -0500

    ts: fix tests compilation

--- ts-koa/src/v2/echo_service.ts
+++ ts-koa/src/v2/echo_service.ts
@@ -2,7 +2,7 @@ import * as service from '../spec/v2/echo_service'
 import * as models from '../spec/v2/models'
 
 export let echoService = (): service.EchoService => {
-    let echoBodyModel = async (params: service.EchoBodyParams): Promise<models.Message> => {
+    let echoBodyModel = async (params: service.EchoBodyModelParams): Promise<models.Message> => {
         return {bool_field: params.body.bool_field, string_field: params.body.string_field}
     }
     
