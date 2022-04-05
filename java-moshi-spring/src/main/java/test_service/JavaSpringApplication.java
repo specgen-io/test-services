c7bef98811d2a3627c12483e87c7d59567cebc05
Anastasiya Nislovskaya <anastasiya@specgen.io>
2022-04-05 19:11:39 +0300

    Added Java Moshi Spring test service, fixed tests (#35)
    * Upgraded specgen
    
    * java-spring: Added Java Moshi Spring test service
    
    * java-spring: Fixed config
    
    * Fixed tests
    
    * java-spring: Fixed build
    
    * Fixed tests
    
    * java-spring: Fixed build and tests
    
    * java-spring: Fixed build
    
    * java-spring: Fixed config
    
    * java-spring: Fixed config
    
    * java-spring: Fixed config
    
    * java-spring: Trying to fix application run
    
    * Upgraded specgen

---
+++ java-moshi-spring/src/main/java/test_service/JavaSpringApplication.java
@@ -0,0 +1,15 @@
+package test_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringApplication.class, args);
+	}
+
+}
---
+++ java-moshi-spring/src/main/java/test_service/services/EchoServiceImpl.java
@@ -0,0 +1,61 @@
+package test_service.services;
+
+import org.springframework.stereotype.Service;
+import test_service.models.*;
+import test_service.services.echo.EchoEverythingResponse;
+import test_service.services.echo.EchoService;
+import test_service.services.echo.SameOperationNameResponse;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Service("EchoService")
+public class EchoServiceImpl implements EchoService {
+	@Override
+	public String echoBodyString(String body) {
+		return body;
+	}
+
+	@Override
+	public Message echoBodyModel(Message body) {
+		return body;
+	}
+
+	@Override
+	public String[] echoBodyArray(String[] body) {
+		return body;
+	}
+
+	@Override
+	public Map<String, String> echoBodyMap(Map<String, String> body) {
+		return body;
+	}
+
+	@Override
+	public Parameters echoQuery(int intQuery, long longQuery, float floatQuery, double doubleQuery, BigDecimal decimalQuery, boolean boolQuery, String stringQuery, String stringOptQuery, String stringDefaultedQuery, String[] stringArrayQuery, UUID uuidQuery, LocalDate dateQuery, LocalDate[] dateArrayQuery, LocalDateTime datetimeQuery, Choice enumQuery) {
+		return new Parameters(intQuery, longQuery, floatQuery, doubleQuery, decimalQuery, boolQuery, stringQuery, stringOptQuery, stringDefaultedQuery, stringArrayQuery, uuidQuery, dateQuery, dateArrayQuery, datetimeQuery, enumQuery);
+	}
+
+	@Override
+	public Parameters echoHeader(int intHeader, long longHeader, float floatHeader, double doubleHeader, BigDecimal decimalHeader, boolean boolHeader, String stringHeader, String stringOptHeader, String stringDefaultedHeader, String[] stringArrayHeader, UUID uuidHeader, LocalDate dateHeader, LocalDate[] dateArrayHeader, LocalDateTime datetimeHeader, Choice enumHeader) {
+		return new Parameters(intHeader, longHeader, floatHeader, doubleHeader, decimalHeader, boolHeader, stringHeader, stringOptHeader, stringDefaultedHeader, stringArrayHeader, uuidHeader, dateHeader, dateArrayHeader, datetimeHeader, enumHeader);
+	}
+
+	@Override
+	public UrlParameters echoUrlParams(int intUrl, long longUrl, float floatUrl, double doubleUrl, BigDecimal decimalUrl, boolean boolUrl, String stringUrl, UUID uuidUrl, LocalDate dateUrl, LocalDateTime datetimeUrl, Choice enumUrl) {
+		return new UrlParameters(intUrl, longUrl, floatUrl, doubleUrl, decimalUrl, boolUrl, stringUrl, uuidUrl, dateUrl, datetimeUrl, enumUrl);
+	}
+
+	@Override
+	public EchoEverythingResponse echoEverything(Message body, float floatQuery, boolean boolQuery, UUID uuidHeader, LocalDateTime datetimeHeader, LocalDate dateUrl, BigDecimal decimalUrl) {
+		return new EchoEverythingResponse.Ok(new Everything(body, floatQuery, boolQuery, uuidHeader, datetimeHeader, dateUrl, decimalUrl));
+	}
+
+	@Override
+	public SameOperationNameResponse sameOperationName() {
+		return new SameOperationNameResponse.Ok();
+	}
+}
